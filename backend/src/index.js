069b5c6b64ce4ab08cb8cb2f66cad4bfe88df895
Igor Tucehl <igor.tuchel0@gmail.com>
2026-03-19 13:15:38 +0000

    added cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import { errorHandlingMiddleware } from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
+
 app.use(
   cors({
     origin: "http://localhost:5173",
