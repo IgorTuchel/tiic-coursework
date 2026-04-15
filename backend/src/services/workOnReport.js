35531035e7935cb2c6606f5c95ed6f7cfc9898ba
igortuchel <ITuchel@pm.me>
2026-04-15 23:16:53 +0100

    added assign to maintanence report service

--- backend/src/services/workOnReport.js
+++ backend/src/services/workOnReport.js
@@ -1,4 +1,5 @@
 import FaultReportAssign from "../models/appdb/faultReportAssign.js";
+import MaintenanceReportAssign from "../models/appdb/maintenanceReportAssign.js";
 
 export async function userAssignedToFaultReport(userID, reportID) {
   const assigned = await FaultReportAssign.findOne({
@@ -58,6 +59,73 @@ export async function unassignUserFromFaultReport(userID, reportID) {
     return {
       success: false,
       message: "Failed to unassign user from fault report",
+    };
+  }
+
+  return { success: true };
+}
+
+export async function userAssignedToMaintenanceReport(userID, reportID) {
+  const assigned = await MaintenanceReportAssign.findOne({
+    where: {
+      userID,
+      maintenanceReportID: reportID,
+    },
+  });
+  if (!assigned) {
+    return false;
+  }
+  return true;
+}
+
+export async function assignUserToMaintenanceReport(userID, reportID) {
+  const existingAssignment = await MaintenanceReportAssign.findOne({
+    where: {
+      userID,
+      maintenanceReportID: reportID,
+    },
+  });
+  if (existingAssignment) {
+    return {
+      success: false,
+      message: "User already assigned to this maintenance report",
+    };
+  }
+
+  const newAssignment = await MaintenanceReportAssign.create({
+    userID,
+    maintenanceReportID: reportID,
+  });
+
+  if (!newAssignment) {
+    return {
+      success: false,
+      message: "Failed to assign user to maintenance report",
+    };
+  }
+
+  return { success: true, data: newAssignment };
+}
+
+export async function unassignUserFromMaintenanceReport(userID, reportID) {
+  const existingAssignment = await MaintenanceReportAssign.findOne({
+    where: {
+      userID,
+      maintenanceReportID: reportID,
+    },
+  });
+  if (!existingAssignment) {
+    return {
+      success: false,
+      message: "User is not assigned to this maintenance report",
+    };
+  }
+
+  const deleted = await existingAssignment.destroy();
+  if (!deleted) {
+    return {
+      success: false,
+      message: "Failed to unassign user from maintenance report",
     };
   }
 
